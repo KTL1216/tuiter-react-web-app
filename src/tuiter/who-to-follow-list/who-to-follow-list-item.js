71e52aebe2323b3ff324e5c36a47a7efe92d7dd9
KTL1216 <kentheolee@gmail.com>
2023-03-05 22:23:03 -0500

    a6

---
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const WhoToFollowListItem = (
+    {
+        who = { userName: 'NASA', handle: 'NASA', avatarIcon: 'nasa.jpg' }
+    }
+) => {
+    return(
+        <li className="list-group-item">
+            <div className="row align-items-center">
+                <div className="col-lg-2 col-xl-2 col-xxl-2">
+                    <img className="rounded-circle" width={50} src={`/images/${who.avatarIcon}`} alt={"Logo"}/>
+                </div>
+                <div className="col-lg-8 col-xl-8 col-xxl-8 text-nowrap">
+                    <div className="fw-bold">
+                        {who.userName}{' '}
+                    </div>
+                    <div>@{who.handle}</div>
+                </div>
+                <div className="col-lg-2 col-xl-2 col-xxl-2">
+                    <button className="btn btn-primary rounded-pill float-end">Follow</button>
+                </div>
+            </div>
+        </li>
+    );
+};
+export default WhoToFollowListItem;
