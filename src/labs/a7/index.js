a37328beb62266ac725d7c50f9dca151493b65e5
KTL1216 <kentheolee@gmail.com>
2023-03-27 01:11:39 -0400

    a7

--- src/labs/a7/index.js
+++ src/labs/a7/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ReduxExamples from "./redux-examples";
 
 const Assignment7 = () => {
- return(
-   <>
-     <h1>Assignment 7</h1>
-   </>
- );
+    return(
+        <>
+            <h1>Assignment 7</h1>
+            <ReduxExamples />
+        </>
+    );
 };
 export default Assignment7;
